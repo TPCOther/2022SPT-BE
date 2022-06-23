f95695ebfa83d94165668f55eae419bf0b141b2e
npcxh <bakanpcxh@gmail.com>
2022-06-23 17:35:42 +0800

    init

---
+++ src/main/java/com/easyorder/EasyorderApplication.java
@@ -0,0 +1,13 @@
+package com.easyorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easyorder/EasyorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
